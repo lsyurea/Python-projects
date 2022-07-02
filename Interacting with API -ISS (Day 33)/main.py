cf067c68d5aae347f33e1acb3f967a7c12d418e3
Lee Si Yuan <lee.si.yuan.20@gmail.com>
2022-07-02 11:42:05 +0800

    modified API to include sunrise/sunset

--- Interacting with API -ISS (Day 33)/main.py
+++ Interacting with API -ISS (Day 33)/main.py
@@ -5,3 +5,13 @@ r.raise_for_status()
 data = r.json()
 position = longitude, latitude = tuple(data['iss_position'].values())
 print(position)
+
+
+parameters = {
+    'lat': latitude,
+    'lng': longitude,
+}
+response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
+response.raise_for_status()
+data = response.json()
+print(data)
